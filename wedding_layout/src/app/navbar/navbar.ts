824e799b89a7bd306c798f0b9fe362e3e8d439b7
SaraswathiS2004 <saraswathis1332004@gmail.com>
2026-07-06 18:34:27 +0530

    Created Angular project

---
+++ wedding_layout/src/app/navbar/navbar.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'nav-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.css',
+})
+export class Navbar {}
